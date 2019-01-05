2364eade3fb1474ffbf4b4032c2daa5072070689
1Cbondy1 <claytonbondy@gmail.com>
2019-01-05 17:54:43 -0600

    built route to scrape npr.org

--- server.js
+++ server.js
@@ -31,21 +31,21 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("http://www.echojs.com/").then(function(response) {
+    axios.get("https://www.npr.org/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function(i, element) {
+      $("h3.title").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
         // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
-          .children("a")
-          .text();
+          .parent("a")
+          .text().trim();
         result.link = $(this)
-          .children("a")
+          .parent("a")
           .attr("href");
   
         // Create a new Article using the `result` object built from scraping
